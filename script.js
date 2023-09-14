39cdbab43d7d9092ce8944b389a13a40e032cce5
Sidney Evers Zuanazzi <sidney.zuanazzi@proway.treina>
2023-09-14 14:32:44 -0300

    alteração exercicio 2

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 function verificar(){
     const idade = document.querySelector(".idade").value;
 
-    const titulo = document.querySelector(".titulidade");
-
-    
-
+    const titulo = document.querySelector(".titulidade");   
 
     if(idade < 18){
-        alert()
+        titulo.innerText = "Você é menor de idade";
     }
     else{
         titulo.innerText = "Você é maior de idade";
