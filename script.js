9ab6a4bb640db00642b6add41ecd4e07bb809b5a
Sidney Evers Zuanazzi <sidneyeverszuanazzi@gmail.com>
2023-09-14 20:52:42 -0300

    media feita

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ function verificar(){
         titulo.innerText = "Você é maior de idade";
     }
 }
+
+function resultadomedia(){
+
+    let nota1 = parseFloat(document.querySelector(".n1").value);
+
+    let nota2 = parseFloat(document.querySelector(".n2").value);
+
+    let nota3 = parseFloat(document.querySelector(".n3").value);
+
+    const frq = parseFloat(document.querySelector(".f1").value);
+
+    const titumedia = document.querySelector(".resultadomedia");
+
+    let todasAsNotas = parseInt((nota1 + nota2 + nota3));
+
+    let mediaNotas = parseInt(todasAsNotas / 3);
+
+    if(mediaNotas >= 7 && frq >= 75) {
+        titumedia.innerText = `Media: ${mediaNotas} --> Aprovado`
+        console.log(mediaNotas);
+    } else {
+        titumedia.innerText = 'Voce foi reprovado'
+        console.log(mediaNotas)
+    }
+}
