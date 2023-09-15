3536fbd08d244237ec1a269cf2f75b46aedceecb
Sidney Evers Zuanazzi <sidneyeverszuanazzi@gmail.com>
2023-09-14 22:13:12 -0300

    IMC pronto e funcionando

--- script.js
+++ script.js
@@ -35,3 +35,49 @@ function resultadomedia(){
         console.log(mediaNotas)
     }
 }
+
+function calculoImcbase(){
+
+
+    let idade = parseFloat(document.querySelector(".idade1").value);
+
+    let peso = parseFloat(document.querySelector(".peso1").value);
+
+    let altura = parseFloat(document.querySelector(".altura1").value);
+
+    var alturaoquadrado = parseFloat(altura*altura);
+
+    var resultadoimc = parseFloat(peso / alturaoquadrado);
+
+    var valormaximoimx = document.querySelector(".valortotalimc");
+
+   if(resultadoimc < 17){
+    valormaximoimx.innerText = `Seu IMC é de ${resultadoimc}, você está muito abaixo do peso!`;
+    console.log(resultadoimc);
+   }
+
+   else if(resultadoimc >= 17 && resultadoimc <= 18.49){
+    valormaximoimx.innerText = `Seu IMC é de ${resultadoimc}, você está abaixo do peso!`;
+    console.log(resultadoimc);
+   }
+
+   else if(resultadoimc >= 18.5 && resultadoimc <= 24.99){
+    valormaximoimx.innerText = `Seu IMC é de ${resultadoimc}, você está com peso normal!`;
+   }
+
+   else if(resultadoimc >= 25 && resultadoimc <= 29.99){
+    valormaximoimx.innerText = `Seu IMC é de ${resultadoimc}, você está acima do peso!`;
+   }
+
+   else if(resultadoimc >= 30 && resultadoimc <= 34.99){
+    valormaximoimx.innerText = `Seu IMC é de ${resultadoimc}, você está com obesidade 1!`;
+   }
+
+   else if(resultadoimc >= 35 && resultadoimc <= 39.99){
+    valormaximoimx.innerText = `Seu IMC é de ${resultadoimc}, você está com obesidade 2!`;
+   }
+
+   else if(resultadoimc > 75){
+    valormaximoimx.innerText = `Seu IMC é de ${resultadoimc}, você está com obesidade 3!`;
+   }
+}
