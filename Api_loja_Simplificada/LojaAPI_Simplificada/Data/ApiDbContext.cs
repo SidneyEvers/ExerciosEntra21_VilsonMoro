2b8c29c310d961c4f2783658ca6476ca4cc7d6d2
SidneyEvers <sidneyeverszuanazzi@gmail.com>
2024-01-19 17:14:57 -0300

    consumir api feito na loja

---
+++ Api_loja_Simplificada/LojaAPI_Simplificada/Data/ApiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LojaAPI_Simplificada.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LojaAPI_Simplificada.Data;
+
+public class ApiDbContext : DbContext
+{
+    public ApiDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    public DbSet<Categoria> Categorias_Tables { get; set; }
+    public DbSet<Produtos> Produtos_Table { get; set; }
+
+}
---
+++ Api_loja_Simplificada/LojaAPI_Simplificada/Program.cs
@@ -0,0 +1,52 @@
+
+using LojaAPI_Simplificada.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LojaAPI_Simplificada
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+           
+
+            // Add services to the container.
+            builder.Services.AddDbContext<ApiDbContext>(opts => opts.
+            UseMySql(builder.Configuration.GetConnectionString("LojaConexaoAPI"),
+            ServerVersion.AutoDetect(builder.Configuration.
+            GetConnectionString("LojaConexaoAPI"))));
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            app.UseCors(options =>
+            {
+                options.AllowAnyHeader();
+                options.AllowAnyMethod();
+                options.AllowAnyOrigin();
+            });
+
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
