ba2aff549e635d6d62b8ed00b4c96285ebdb717f
sidney.zuanazzi <sidney.zuanazzi@YELLOW06>
2024-01-24 13:22:45 -0300

    nova atualização

--- Api_loja_Simplificada/LojaAPI_Simplificada/Config/Configuracoes.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace LojaAPI_Simplificada.Config;
-
-public class Configuracoes
-{
-    public static string Secret = "fedaf7d8863b48e197b9287d492b708e";
-}
--- Api_loja_Simplificada/LojaAPI_Simplificada/Dto/userDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LojaAPI_Simplificada.Dto;
-
-public class userDto
-{
-    public string Username { get; set; }
-    public string Password { get; set; }
-}
